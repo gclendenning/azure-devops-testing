44d8b8bcb20381ba6f4db81acd70cd8ffcc3680e
gclendenning <64431984+gclendenning@users.noreply.github.com>
2023-05-18 15:05:30 +0100

    Test getting python version with tags

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='sample_package',
+    version='0.1.0',
+    author='',
+    author_email='',
+    description='Sample project',
+    entry_points={},
+    packages=find_packages('sample_package'),
+    install_requires=[],
+    include_package_data=True,
+    url='https://github.com/gclendenning/azure-devops-testing',
+)
