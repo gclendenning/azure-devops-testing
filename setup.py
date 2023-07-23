26656a1db90d2244601bb0107be86e013a66d5b1
gclendenning <64431984+gclendenning@users.noreply.github.com>
2023-07-23 16:18:24 +0100

    Try without release-

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sample_package',
-    version='0.1.1',
+    version='0.1.2',
     author='',
     author_email='',
     description='Sample project',
